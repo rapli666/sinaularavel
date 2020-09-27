992c2570fa9948425c0e775385d3bc0f66affde5
rapli666 <raflim777@gmail.com>
2020-09-27 20:48:03 +0700

    Tugas Blade Tamplate Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('master.master')
+@section('title', 'Laravel')
+
+@section('content')
+<div class="jumbotron jmb">
+	<div style="margin-top: 15px; margin-left: 10px;">
+		<h1 class="home">Samlekum Mamank</h1>
+		</div>
+		<p  style="margin-top: 40px; font-size: 35px;" class="p">Odading mang oleh rasanya seperti anda menjadi ironman</p>
+		
+		
+</div>
+@endsection
---
+++ resources/views/master/master.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<title>@yield('title')</title>
+	<style type="text/css">
+		@import url('https://fonts.googleapis.com/css2?family=Charm:wght@700&display=swap');
+		@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap');
+		@import url('https://fonts.googleapis.com/css2?family=Cinzel:wght@600&display=swap');
+		.navbar {
+			background-color: black;
+		}
+		.navss {
+			color: white;
+			margin-right: 10px;
+			font-family: Charm;
+		}
+		.navs {
+			color: white;
+			font-family: 
+		}
+		.jmb{
+			background-color: #e8e8e8;
+
+		}
+		body {
+			background-color: #ecf4f3;
+		}
+
+		.home{
+						font-family: Dancing Script;
+		}
+		.p{
+			font-family: Cinzel
+		}
+
+	</style>
+</head>
+<body>
+	@include('master._navigasi')
+	@yield('content')
+</body>
+</html>
